380cf436e7db690148847abb772c9c1ddae03e75
ThomasBerinde <Berinde.Da.Thomas@student.utcluj.ro>
2022-12-28 22:41:49 +0200

    login + create order functionalities finished

--- src/main/java/com/example/seproject2022/controller/OrderController.java
+++ src/main/java/com/example/seproject2022/controller/OrderController.java
@@ -1,9 +1,18 @@
 package com.example.seproject2022.controller;
 
+import com.example.seproject2022.model.dto.CreateOrderRequestDto;
 import com.example.seproject2022.service.OrderService;
+import com.example.seproject2022.service.ValidatorService;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,4 +22,13 @@ import lombok.RequiredArgsConstructor;
 public class OrderController {
 
     private final OrderService orderService;
+    private final ValidatorService validatorService;
+
+    @PostMapping()
+    public ResponseEntity<String> create(@RequestHeader(value = "jwt", required = false) String jwt,
+                                         @RequestBody CreateOrderRequestDto createOrderRequestDto,
+                                         HttpServletRequest request) {
+        validatorService.validateIsUser(jwt, request.getRequestURI());
+        return new ResponseEntity<>(orderService.createOrder(createOrderRequestDto, request.getRequestURI()), HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/seproject2022/service/OrderService.java
+++ src/main/java/com/example/seproject2022/service/OrderService.java
@@ -1,9 +1,9 @@
 package com.example.seproject2022.service;
 
 import com.example.seproject2022.model.dto.CreateOrderRequestDto;
-import com.example.seproject2022.model.dto.CreateOrderResponseDto;
 
 public interface OrderService {
 
-    CreateOrderResponseDto createOrder(CreateOrderRequestDto requestDto);
+    String createOrder(CreateOrderRequestDto createOrderRequestDto,
+                       String uri);
 }
