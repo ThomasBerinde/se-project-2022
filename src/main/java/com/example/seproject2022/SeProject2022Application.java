bcf5f6050235f667dcaaf5149f52a4383bab6608
Giuni <bodea.georgiana001@gmail.com>
2022-12-29 01:48:32 +0200

    Added requests for product and category

--- src/main/java/com/example/seproject2022/SeProject2022Application.java
+++ src/main/java/com/example/seproject2022/SeProject2022Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SeProject2022Application {
-
+    //de facut validari pe DTO-uri sa nu se poate introduce date eronate!!!!!!!11
     public static void main(String[] args) {
         SpringApplication.run(SeProject2022Application.class, args);
     }
